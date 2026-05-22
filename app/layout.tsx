0a8e5c4c84a8b8dc0b2f4179bb9ab6f0e554f36d
TechNuo <jinnuonoel@gmail.com>
2026-05-22 19:52:50 +0200

    fix: add mobile-web-app-capable meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
